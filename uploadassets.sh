ba0cf798090e9dc0b4ce0c9ba6394722a2cee7eb
Erin Drummond <erin.dru@gmail.com>
2012-06-14 12:44:29 +1200

    add basic assets upload script

---
+++ uploadassets.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#parameters
+bucket_name="cdn.openseedbox.com"
+
+echo -n "Checking s3cmd is installed..." 
+have_s3cmd=`which s3cmd`
+if [ "$have_s3cmd" == "" ]; then
+	echo -e "\nNo s3cmd detected, installing."
+	sudo apt-get install s3cmd
+else
+	echo " it is."
+fi
+
+echo -n "Checking s3cmd is configured..."
+if [ ! -f "$HOME/.s3cfg" ]; then
+	s3cmd --configure
+else 
+	echo -n " it is."
+fi
+
+echo "Syncing /public with $bucket_name/public"
+s3cmd sync "public/" "s3://$bucket_name/public/"
+
+echo "All done."
