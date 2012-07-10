6ff5a346463140e94621c336b7fef926f9e06f5c
Erin Drummond <erin.dru@gmail.com>
2012-07-10 21:29:44 +1200

    Make ping job not fire in dev, also remove extra error message on client plan page

--- app/code/jobs/PingJob.java
+++ app/code/jobs/PingJob.java
@@ -1,6 +1,7 @@
 package code.jobs;
 
 import play.Logger;
+import play.Play;
 import play.jobs.Every;
 import play.jobs.Job;
 import play.libs.WS;
@@ -11,11 +12,13 @@ public class PingJob extends Job {
 
 	@Override
 	public void doJob() throws Exception {
-		//Ping the server to keep the Heroku dyno alive
-		Logger.info("Pinging client.openseedbox.com to keep the server alive...");
-		HttpResponse res = WS.url("http://client.openseedbox.com/ping?ext=json").get();
-		String response = res.getJson().getAsJsonObject().get("data").getAsString();
-		Logger.info("Server responded: %s", response);
+		//Ping the server to keep the Heroku dyno alive (only in prod mode though)
+		if (Play.mode != Play.Mode.DEV) {
+			Logger.info("Pinging client.openseedbox.com to keep the server alive...");
+			HttpResponse res = WS.url("http://client.openseedbox.com/ping?ext=json").get();
+			String response = res.getJson().getAsJsonObject().get("data").getAsString();
+			Logger.info("Server responded: %s", response);
+		}
 	}
 	
 }
