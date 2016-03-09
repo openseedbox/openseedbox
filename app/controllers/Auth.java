fee38968d3eba0ecacbc5dd1dc65652ede536165
Erin Drummond <erin.dru@gmail.com>
2016-03-09 22:32:49 +1300

    Fix issue where docker image would not work

--- app/controllers/Auth.java
+++ app/controllers/Auth.java
@@ -14,10 +14,10 @@ import play.libs.WS.HttpResponse;
 
 public class Auth extends Base {
 
-        private static final String GOOGLE_TOKEN_ENDPOINT = "https://accounts.google.com/o/oauth2/tokeninfo";
+	private static final String GOOGLE_TOKEN_ENDPOINT = "https://accounts.google.com/o/oauth2/tokeninfo";
 
 	public static void login() {
-                renderArgs.put("clientId", Config.getGoogleClientId());
+        renderArgs.put("clientId", Config.getGoogleClientId());
 		renderTemplate("auth/login.html");
 	}
 
@@ -28,39 +28,39 @@ public class Auth extends Base {
 	}
 
 	public static void authenticate(String id_token) throws Exception {
-                HttpResponse googleResponse = WS.url(GOOGLE_TOKEN_ENDPOINT).setParameter("id_token", id_token).getAsync().get();
+        HttpResponse googleResponse = WS.url(GOOGLE_TOKEN_ENDPOINT).setParameter("id_token", id_token).getAsync().get();
 
-                JsonObject body = googleResponse.getJson().getAsJsonObject();
+        JsonObject body = googleResponse.getJson().getAsJsonObject();
 
-                String emailAddress = body.get("email").getAsString();
+        String emailAddress = body.get("email").getAsString();
 
-                User u = User.findByEmailAddress(emailAddress);
+        User u = User.findByEmailAddress(emailAddress);
 
-                if (u == null) {
-                    //create new user
-                    u = new User();
-                    u.setEmailAddress(emailAddress);
-                    // also set displayname, as it's required (fixes /admin/edituser)
-                    u.setDisplayName(emailAddress);
-                    u.setAvatarUrl(String.format("https://www.gravatar.com/avatar/%s",
-                                    DigestUtils.md5Hex(u.getEmailAddress())));
-                    u.setLastAccess(new Date());
-                    
-                    //if this is the very first user, set them as admin
-                    boolean isFirstUser = User.count() == 0;
-                    u.setAdmin(isFirstUser);
-                    
-                    u.save();
+        if (u == null) {
+            //create new user
+            u = new User();
+            u.setEmailAddress(emailAddress);
+            // also set displayname, as it's required (fixes /admin/edituser)
+            u.setDisplayName(emailAddress);
+            u.setAvatarUrl(String.format("https://www.gravatar.com/avatar/%s",
+                            DigestUtils.md5Hex(u.getEmailAddress())));
+            u.setLastAccess(new Date());
+            
+            //if this is the very first user, set them as admin
+            boolean isFirstUser = User.count() == 0;
+            u.setAdmin(isFirstUser);
+            
+            u.save();
 
-                    // reload user and signin automatically
-                    u = User.findByEmailAddress(emailAddress);
-                    session.put("currentUserId", u.getId());
-                } else {
-                    //login user
-                    u.setLastAccess(new Date());
-                    u.save();
-                    session.put("currentUserId", u.getId());
-                }
-                redirect("/client");
+            // reload user and signin automatically
+            u = User.findByEmailAddress(emailAddress);
+            session.put("currentUserId", u.getId());
+        } else {
+            //login user
+            u.setLastAccess(new Date());
+            u.save();
+            session.put("currentUserId", u.getId());
+        }
+        redirect("/client");
 	}
 }
