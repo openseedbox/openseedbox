67ea24e7ae73eab6fe2c65f2e91ec164f554f695
Szabolcs Hubai <szab.hu@gmail.com>
2017-06-02 02:59:10 +0200

    Docker: make run.sh universal
    and use WORKDIR for being in the right dir

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cd /src/openseedbox
 
 APPLICATION_SECRET=`grep "application.secret=NOT_GENERATED" conf/application.conf`
+GOOGLE_CLIENTID_NEEDED=`grep "google.clientid=" conf/application.conf`
 
 if [ "$APPLICATION_SECRET"  != "" ]; then
 	echo "Play secret has not been generated; generating"
 	/play/play secret
 fi
 
-if [ "$GOOGLE_CLIENTID" == "" ]; then
+if [ "$GOOGLE_CLIENTID" == "" -a -n "$GOOGLE_CLIENTID_NEEDED" ]; then
 	echo "You need to specify your Google ClientID in the GOOGLE_CLIENTID environment variable or you wont be able to log in"
 	exit 1
 fi
