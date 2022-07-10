openseedbox/openseedbox

commit 1a45c00e8ca7c55f45020cf7034c37b5b7c8bca2

Docker: rewrite to be more path-based routing friendly (#82, #70)
Deploy to Tomcat as an exploded WAR without the following
functionalities (from docker/run.sh):
- Play secret generation
- checking for missing Google ClientID
- session cookie name generation

docker/Dockerfile
@@ -1,0 +1,77 @@
+# syntax=docker/dockerfile:1
+
+FROM openseedbox/base as builder
+
+RUN apt update -qq && apt install -y --no-install-recommends \
+	git ca-certificates python tomcat-jakartaee-migration;
+
+RUN bash -c "for repo in openseedbox{-common,}; do echo updating git for \$repo; cd /src/\$repo; git pull --ff-only origin master; done"
+RUN bash -c "for repo in openseedbox{-common,}; do echo updating play deps for \$repo; cd /src/\$repo; /play/play deps --sync --forProd; done"
+
+COPY application.conf /src/openseedbox/conf/application.conf
+
+WORKDIR /src
+RUN /play/play war openseedbox -o war
+# Remove old Play! bundled jars
+RUN bash -c "rm -fv war/WEB-INF/lib/{postgresql-9,log4j-1,slf4j-log4j12-1}.*.jar;"
+
+# Remove irrelevant application directories
+RUN bash -c "rm -rfv war/WEB-INF/application/{docker,test,javadoc,lib}"
+
+# (Optional) Suppress log messages "At least one JAR was scanned for TLDs yet contained no TLDs." during Tomcat startup
+RUN mkdir war/META-INF; \
+	echo '<?xml version="1.0" encoding="UTF-8"?>\n\
+<Context>\n\
+    <JarScanner>\n\
+        <JarScanFilter tldSkip="*.*"/>\n\
+    </JarScanner>\n\
+</Context>' > war/META-INF/context.xml;
+# Only necessary if using Tomcat >= 10.0 due to package name change from javax.* to jakarta.*
+ENV PATH $JAVA_HOME/bin:$PATH
+RUN javax2jakarta war jakarta-war; rm -rf war; mv -v jakarta-war war
+
+
+FROM debian:bullseye-slim
+# Copy Java
+COPY --from=builder /java /java
+ENV JAVA_HOME=/java
+
+
+# Mimic Tomcat image (copy-paste from https://github.com/docker-library/tomcat)
+ENV CATALINA_HOME /tomcat
+ENV PATH $CATALINA_HOME/bin:$PATH
+RUN mkdir -p "$CATALINA_HOME"
+WORKDIR $CATALINA_HOME
+
+# let "Tomcat Native" live somewhere isolated
+ENV TOMCAT_NATIVE_LIBDIR $CATALINA_HOME/native-jni-lib
+ENV LD_LIBRARY_PATH ${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$TOMCAT_NATIVE_LIBDIR
+
+COPY --from=builder $CATALINA_HOME $CATALINA_HOME
+RUN set -eux; \
+	apt-get update; \
+	xargs -rt apt-get install -y --no-install-recommends < "$TOMCAT_NATIVE_LIBDIR/.dependencies.txt"; \
+	rm -rf /var/lib/apt/lists/*
+
+# verify Tomcat Native is working properly
+RUN set -eux; \
+	nativeLines="$(catalina.sh configtest 2>&1)"; \
+	nativeLines="$(echo "$nativeLines" | grep 'Apache Tomcat Native')"; \
+	nativeLines="$(echo "$nativeLines" | sort -u)"; \
+	if ! echo "$nativeLines" | grep -E 'INFO: Loaded( APR based)? Apache Tomcat Native library' >&2; then \
+		echo >&2 "$nativeLines"; \
+		exit 1; \
+	fi
+
+EXPOSE 8080
+CMD ["catalina.sh", "run"]
+
+# Default values for config environment variables
+ENV OPENSEEDBOX_JDBC_URL=jdbc:postgresql://openseedboxdb/openseedbox
+ENV OPENSEEDBOX_JDBC_DRIVER=org.postgresql.Driver
+ENV OPENSEEDBOX_JDBC_USER=openseedbox
+ENV OPENSEEDBOX_JDBC_PASS=openseedbox
+ENV OPENSEEDBOX_CONTEXT_PATH=
+
+# Copy WAR file from builder container to the final image
+COPY --from=builder /src/war $CATALINA_HOME/webapps/ROOT

docker/application.conf
@@ -1,6 +1,6 @@
 application.name=openseedbox
 application.mode=prod
-application.secret=NOT_GENERATED
+application.secret=${APPLICATION_SECRET}
 date.format=yyyy-MM-dd
 
 application.log=DEBUG
@@ -20,7 +20,7 @@ siena.ddl=update
 http.port=9000
 XForwardedSupport=127.0.0.1
 application.session.maxAge=3d
-application.session.cookie=NOT_GENERATED
+application.session.cookie=${OPENSEEDBOX_SESSION_COOKIE_NAME}
 webservice=com.openseedbox.code.ws.WSAsyncWithCustomSSLContext
 
 google.clientid=${GOOGLE_CLIENTID}

docker/Dockerfile.tpl
@@ -1,103 +1,0 @@
-FROM balenalib/#{ARCH}-debian:buster
-
-ENTRYPOINT [ "/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf" ]
-
-# Default values for config environment variables
-ENV OPENSEEDBOX_JDBC_URL=jdbc:postgresql://openseedboxdb/openseedbox
-ENV OPENSEEDBOX_JDBC_DRIVER=org.postgresql.Driver
-ENV OPENSEEDBOX_JDBC_USER=openseedbox
-ENV OPENSEEDBOX_JDBC_PASS=openseedbox
-ENV OPENSEEDBOX_CONTEXT_PATH=
-
-EXPOSE 443
-
-ENV JAVA_HOME=/java
-
-#{BALENA_CROSSBUILD_BEGIN}
-
-# See https://github.com/resin-io-library/base-images/issues/273
-# "Errors installing OpenJDK due to unexistent man pages directory"
-#RUN mkdir /usr/share/man/man1
-
-# Install runtime packages
-RUN apt-get -qq update \
-	&& apt-get -qq install -y --no-install-recommends \
-		curl wget unzip git \
-		python supervisor \
-		zlibc zlib1g \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install Temurin JDK from Adoptium
-RUN apt-get -qq update && apt-get -qq install -y gnupg \
-	&& wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | apt-key add - \
-	&& echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list \
-	&& apt-get -qq update \
-	&& apt-get -qq install -y temurin-11-jdk \
-	&& jlink --add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.prefs,java.rmi,java.scripting,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,jdk.crypto.cryptoki,jdk.jdi,jdk.management,jdk.unsupported --output /java/ --strip-debug --no-man-pages --compress=2 \
-	&& apt-get -qq purge -y gnupg temurin-11-jdk \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install play
-ENV PLAY_VERSION=1.4.6
-RUN wget -q "https://downloads.typesafe.com/play/${PLAY_VERSION}/play-${PLAY_VERSION}.zip" \
-	&& unzip -q play-${PLAY_VERSION}.zip -x play-${PLAY_VERSION}/documentation/* play-${PLAY_VERSION}/samples-and-tests/* \
-	&& mv /play-${PLAY_VERSION} /play \
-	&& rm play-${PLAY_VERSION}.zip
-
-# Install siena module to play
-RUN echo y | /play/play install siena-2.0.7 || echo "Downloading directly ... " \
-	&& curl -S -s -L -o siena-2.0.7.zip "https://www.playframework.com/modules/siena-2.0.7.zip" \
-	&& for zipfile in *.zip; do module="${zipfile%.zip}"; unzip -d /play/modules/"$module" "$zipfile"; rm "$zipfile"; done;
-
-WORKDIR /src
-
-# Check out code we rely on and install play! dependencies
-RUN git clone -q https://github.com/openseedbox/openseedbox-common \
-	&& git clone --depth=1 -q https://github.com/openseedbox/openseedbox \
-	&& /play/play deps openseedbox-common --sync \
-	&& /play/play deps openseedbox --sync
-
-# Download and compile nginx
-RUN apt-get -qq update \
-	&& apt-get -qq install -y \
-		#{BUILD_DEPS} \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-	&& git clone --depth=1 -q https://github.com/evanmiller/mod_zip \
-	&& git clone --depth=1 -q https://github.com/agentzh/headers-more-nginx-module \
-	&& wget -q -O nginx.tar.gz http://nginx.org/download/nginx-1.20.2.tar.gz \
-	&& tar -xf nginx.tar.gz \
-	&& cd nginx* \
-	&& ./configure --with-http_ssl_module --add-module=/src/mod_zip/ \
-		--prefix=/etc/nginx --conf-path=/etc/nginx/nginx.conf \
-		--error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid \
-		--http-log-path=/var/log/nginx/access.log --lock-path=/var/lock/nginx.lock \
-		--sbin-path=/usr/sbin/nginx --add-module=/src/headers-more-nginx-module \
-	&& make \
-	&& make -s install \
-	&& cd .. \
-	&& rm -fr nginx* mod_zip headers-more-nginx-module \
-	&& apt-get -qq purge -y \
-		#{BUILD_DEPS} \
-	&& apt-get -y autoremove \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-#create SSL keys
-RUN openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=None/L=None/O=None/CN=openseedbox" \
-	-keyout /src/openseedbox/conf/host.key \
-	-out /src/openseedbox/conf/host.cert
-
-#{BALENA_CROSSBUILD_END}
-
-#copy config files
-COPY application.conf /src/openseedbox/conf/application.conf
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
-#copy run scripts
-COPY run.sh /run.sh
-COPY stop-supervisord.sh /
-
-WORKDIR /src/openseedbox

docker/aarch64.Dockerfile
@@ -1,99 +1,0 @@
-FROM balenalib/aarch64-debian:buster
-
-ENTRYPOINT [ "/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf" ]
-
-# Default values for config environment variables
-ENV OPENSEEDBOX_JDBC_URL=jdbc:postgresql://openseedboxdb/openseedbox
-ENV OPENSEEDBOX_JDBC_DRIVER=org.postgresql.Driver
-ENV OPENSEEDBOX_JDBC_USER=openseedbox
-ENV OPENSEEDBOX_JDBC_PASS=openseedbox
-ENV OPENSEEDBOX_CONTEXT_PATH=
-
-EXPOSE 443
-
-ENV JAVA_HOME=/java
-
-# See https://github.com/resin-io-library/base-images/issues/273
-# "Errors installing OpenJDK due to unexistent man pages directory"
-#RUN mkdir /usr/share/man/man1
-
-# Install runtime packages
-RUN apt-get -qq update \
-	&& apt-get -qq install -y --no-install-recommends \
-		curl wget unzip git \
-		python supervisor \
-		zlibc zlib1g \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install Temurin JDK from Adoptium
-RUN apt-get -qq update && apt-get -qq install -y gnupg \
-	&& wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | apt-key add - \
-	&& echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list \
-	&& apt-get -qq update \
-	&& apt-get -qq install -y temurin-11-jdk \
-	&& jlink --add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.prefs,java.rmi,java.scripting,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,jdk.crypto.cryptoki,jdk.jdi,jdk.management,jdk.unsupported --output /java/ --strip-debug --no-man-pages --compress=2 \
-	&& apt-get -qq purge -y gnupg temurin-11-jdk \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install play
-ENV PLAY_VERSION=1.4.6
-RUN wget -q "https://downloads.typesafe.com/play/${PLAY_VERSION}/play-${PLAY_VERSION}.zip" \
-	&& unzip -q play-${PLAY_VERSION}.zip -x play-${PLAY_VERSION}/documentation/* play-${PLAY_VERSION}/samples-and-tests/* \
-	&& mv /play-${PLAY_VERSION} /play \
-	&& rm play-${PLAY_VERSION}.zip
-
-# Install siena module to play
-RUN echo y | /play/play install siena-2.0.7 || echo "Downloading directly ... " \
-	&& curl -S -s -L -o siena-2.0.7.zip "https://www.playframework.com/modules/siena-2.0.7.zip" \
-	&& for zipfile in *.zip; do module="${zipfile%.zip}"; unzip -d /play/modules/"$module" "$zipfile"; rm "$zipfile"; done;
-
-WORKDIR /src
-
-# Check out code we rely on and install play! dependencies
-RUN git clone -q https://github.com/openseedbox/openseedbox-common \
-	&& git clone --depth=1 -q https://github.com/openseedbox/openseedbox \
-	&& /play/play deps openseedbox-common --sync \
-	&& /play/play deps openseedbox --sync
-
-# Download and compile nginx
-RUN apt-get -qq update \
-	&& apt-get -qq install -y \
-		build-essential libpcre3-dev libssl-dev zlib1g-dev \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-	&& git clone --depth=1 -q https://github.com/evanmiller/mod_zip \
-	&& git clone --depth=1 -q https://github.com/agentzh/headers-more-nginx-module \
-	&& wget -q -O nginx.tar.gz http://nginx.org/download/nginx-1.20.2.tar.gz \
-	&& tar -xf nginx.tar.gz \
-	&& cd nginx* \
-	&& ./configure --with-http_ssl_module --add-module=/src/mod_zip/ \
-		--prefix=/etc/nginx --conf-path=/etc/nginx/nginx.conf \
-		--error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid \
-		--http-log-path=/var/log/nginx/access.log --lock-path=/var/lock/nginx.lock \
-		--sbin-path=/usr/sbin/nginx --add-module=/src/headers-more-nginx-module \
-	&& make \
-	&& make -s install \
-	&& cd .. \
-	&& rm -fr nginx* mod_zip headers-more-nginx-module \
-	&& apt-get -qq purge -y \
-		build-essential libpcre3-dev libssl-dev zlib1g-dev \
-	&& apt-get -y autoremove \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-#create SSL keys
-RUN openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=None/L=None/O=None/CN=openseedbox" \
-	-keyout /src/openseedbox/conf/host.key \
-	-out /src/openseedbox/conf/host.cert
-
-#copy config files
-COPY application.conf /src/openseedbox/conf/application.conf
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
-#copy run scripts
-COPY run.sh /run.sh
-COPY stop-supervisord.sh /
-
-WORKDIR /src/openseedbox

docker/amd64.Dockerfile
@@ -1,99 +1,0 @@
-FROM balenalib/amd64-debian:buster
-
-ENTRYPOINT [ "/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf" ]
-
-# Default values for config environment variables
-ENV OPENSEEDBOX_JDBC_URL=jdbc:postgresql://openseedboxdb/openseedbox
-ENV OPENSEEDBOX_JDBC_DRIVER=org.postgresql.Driver
-ENV OPENSEEDBOX_JDBC_USER=openseedbox
-ENV OPENSEEDBOX_JDBC_PASS=openseedbox
-ENV OPENSEEDBOX_CONTEXT_PATH=
-
-EXPOSE 443
-
-ENV JAVA_HOME=/java
-
-# See https://github.com/resin-io-library/base-images/issues/273
-# "Errors installing OpenJDK due to unexistent man pages directory"
-#RUN mkdir /usr/share/man/man1
-
-# Install runtime packages
-RUN apt-get -qq update \
-	&& apt-get -qq install -y --no-install-recommends \
-		curl wget unzip git \
-		python supervisor \
-		zlibc zlib1g \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install Temurin JDK from Adoptium
-RUN apt-get -qq update && apt-get -qq install -y gnupg \
-	&& wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | apt-key add - \
-	&& echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list \
-	&& apt-get -qq update \
-	&& apt-get -qq install -y temurin-11-jdk \
-	&& jlink --add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.prefs,java.rmi,java.scripting,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,jdk.crypto.cryptoki,jdk.jdi,jdk.management,jdk.unsupported --output /java/ --strip-debug --no-man-pages --compress=2 \
-	&& apt-get -qq purge -y gnupg temurin-11-jdk \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install play
-ENV PLAY_VERSION=1.4.6
-RUN wget -q "https://downloads.typesafe.com/play/${PLAY_VERSION}/play-${PLAY_VERSION}.zip" \
-	&& unzip -q play-${PLAY_VERSION}.zip -x play-${PLAY_VERSION}/documentation/* play-${PLAY_VERSION}/samples-and-tests/* \
-	&& mv /play-${PLAY_VERSION} /play \
-	&& rm play-${PLAY_VERSION}.zip
-
-# Install siena module to play
-RUN echo y | /play/play install siena-2.0.7 || echo "Downloading directly ... " \
-	&& curl -S -s -L -o siena-2.0.7.zip "https://www.playframework.com/modules/siena-2.0.7.zip" \
-	&& for zipfile in *.zip; do module="${zipfile%.zip}"; unzip -d /play/modules/"$module" "$zipfile"; rm "$zipfile"; done;
-
-WORKDIR /src
-
-# Check out code we rely on and install play! dependencies
-RUN git clone -q https://github.com/openseedbox/openseedbox-common \
-	&& git clone --depth=1 -q https://github.com/openseedbox/openseedbox \
-	&& /play/play deps openseedbox-common --sync \
-	&& /play/play deps openseedbox --sync
-
-# Download and compile nginx
-RUN apt-get -qq update \
-	&& apt-get -qq install -y \
-		build-essential libpcre3-dev libssl-dev zlib1g-dev \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-	&& git clone --depth=1 -q https://github.com/evanmiller/mod_zip \
-	&& git clone --depth=1 -q https://github.com/agentzh/headers-more-nginx-module \
-	&& wget -q -O nginx.tar.gz http://nginx.org/download/nginx-1.20.2.tar.gz \
-	&& tar -xf nginx.tar.gz \
-	&& cd nginx* \
-	&& ./configure --with-http_ssl_module --add-module=/src/mod_zip/ \
-		--prefix=/etc/nginx --conf-path=/etc/nginx/nginx.conf \
-		--error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid \
-		--http-log-path=/var/log/nginx/access.log --lock-path=/var/lock/nginx.lock \
-		--sbin-path=/usr/sbin/nginx --add-module=/src/headers-more-nginx-module \
-	&& make \
-	&& make -s install \
-	&& cd .. \
-	&& rm -fr nginx* mod_zip headers-more-nginx-module \
-	&& apt-get -qq purge -y \
-		build-essential libpcre3-dev libssl-dev zlib1g-dev \
-	&& apt-get -y autoremove \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-#create SSL keys
-RUN openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=None/L=None/O=None/CN=openseedbox" \
-	-keyout /src/openseedbox/conf/host.key \
-	-out /src/openseedbox/conf/host.cert
-
-#copy config files
-COPY application.conf /src/openseedbox/conf/application.conf
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
-#copy run scripts
-COPY run.sh /run.sh
-COPY stop-supervisord.sh /
-
-WORKDIR /src/openseedbox

docker/armv7hf.Dockerfile
@@ -1,103 +1,0 @@
-FROM balenalib/armv7hf-debian:buster
-
-ENTRYPOINT [ "/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf" ]
-
-# Default values for config environment variables
-ENV OPENSEEDBOX_JDBC_URL=jdbc:postgresql://openseedboxdb/openseedbox
-ENV OPENSEEDBOX_JDBC_DRIVER=org.postgresql.Driver
-ENV OPENSEEDBOX_JDBC_USER=openseedbox
-ENV OPENSEEDBOX_JDBC_PASS=openseedbox
-ENV OPENSEEDBOX_CONTEXT_PATH=
-
-EXPOSE 443
-
-ENV JAVA_HOME=/java
-
-RUN [ "cross-build-start" ]
-
-# See https://github.com/resin-io-library/base-images/issues/273
-# "Errors installing OpenJDK due to unexistent man pages directory"
-#RUN mkdir /usr/share/man/man1
-
-# Install runtime packages
-RUN apt-get -qq update \
-	&& apt-get -qq install -y --no-install-recommends \
-		curl wget unzip git \
-		python supervisor \
-		zlibc zlib1g \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install Temurin JDK from Adoptium
-RUN apt-get -qq update && apt-get -qq install -y gnupg \
-	&& wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | apt-key add - \
-	&& echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list \
-	&& apt-get -qq update \
-	&& apt-get -qq install -y temurin-11-jdk \
-	&& jlink --add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.prefs,java.rmi,java.scripting,java.security.jgss,java.security.sasl,java.sql,java.sql.rowset,java.transaction.xa,java.xml,jdk.crypto.cryptoki,jdk.jdi,jdk.management,jdk.unsupported --output /java/ --strip-debug --no-man-pages --compress=2 \
-	&& apt-get -qq purge -y gnupg temurin-11-jdk \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# Install play
-ENV PLAY_VERSION=1.4.6
-RUN wget -q "https://downloads.typesafe.com/play/${PLAY_VERSION}/play-${PLAY_VERSION}.zip" \
-	&& unzip -q play-${PLAY_VERSION}.zip -x play-${PLAY_VERSION}/documentation/* play-${PLAY_VERSION}/samples-and-tests/* \
-	&& mv /play-${PLAY_VERSION} /play \
-	&& rm play-${PLAY_VERSION}.zip
-
-# Install siena module to play
-RUN echo y | /play/play install siena-2.0.7 || echo "Downloading directly ... " \
-	&& curl -S -s -L -o siena-2.0.7.zip "https://www.playframework.com/modules/siena-2.0.7.zip" \
-	&& for zipfile in *.zip; do module="${zipfile%.zip}"; unzip -d /play/modules/"$module" "$zipfile"; rm "$zipfile"; done;
-
-WORKDIR /src
-
-# Check out code we rely on and install play! dependencies
-RUN git clone -q https://github.com/openseedbox/openseedbox-common \
-	&& git clone --depth=1 -q https://github.com/openseedbox/openseedbox \
-	&& /play/play deps openseedbox-common --sync \
-	&& /play/play deps openseedbox --sync
-
-# Download and compile nginx
-RUN apt-get -qq update \
-	&& apt-get -qq install -y \
-		build-essential libpcre3-dev libssl-dev zlib1g-dev \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-	&& git clone --depth=1 -q https://github.com/evanmiller/mod_zip \
-	&& git clone --depth=1 -q https://github.com/agentzh/headers-more-nginx-module \
-	&& wget -q -O nginx.tar.gz http://nginx.org/download/nginx-1.20.2.tar.gz \
-	&& tar -xf nginx.tar.gz \
-	&& cd nginx* \
-	&& ./configure --with-http_ssl_module --add-module=/src/mod_zip/ \
-		--prefix=/etc/nginx --conf-path=/etc/nginx/nginx.conf \
-		--error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid \
-		--http-log-path=/var/log/nginx/access.log --lock-path=/var/lock/nginx.lock \
-		--sbin-path=/usr/sbin/nginx --add-module=/src/headers-more-nginx-module \
-	&& make \
-	&& make -s install \
-	&& cd .. \
-	&& rm -fr nginx* mod_zip headers-more-nginx-module \
-	&& apt-get -qq purge -y \
-		build-essential libpcre3-dev libssl-dev zlib1g-dev \
-	&& apt-get -y autoremove \
-	&& apt-get -y clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-#create SSL keys
-RUN openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=None/L=None/O=None/CN=openseedbox" \
-	-keyout /src/openseedbox/conf/host.key \
-	-out /src/openseedbox/conf/host.cert
-
-RUN [ "cross-build-end" ]
-
-#copy config files
-COPY application.conf /src/openseedbox/conf/application.conf
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
-#copy run scripts
-COPY run.sh /run.sh
-COPY stop-supervisord.sh /
-
-WORKDIR /src/openseedbox

commit 2230efd296a8deb5932a23a94b857da48857b347

Docker: add missing features from the removed docker/run.sh script (#82)
- Play secret generation
- checking for missing Google ClientID
- session cookie name generation

Control Group magic and Java's "-XshowSettings" were intentionally left
out.

Fixes: 1a45c00e8ca7 ("Docker: rewrite to be more path-based routing
friendly (#82, #70)")

app/com/openseedbox/jobs/BootstrapGoogleClientIdCheck.java
@@ -1,0 +1,21 @@
+package com.openseedbox.jobs;
+
+import com.openseedbox.Config;
+import play.exceptions.ConfigurationException;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+
+@OnApplicationStart
+public class BootstrapGoogleClientIdCheck extends Job {
+
+    @Override
+    public void doJob() throws Exception {
+        String clientId = Config.getGoogleClientId();
+        if (clientId == null || clientId.isEmpty() || clientId.contains("${GOOGLE_CLIENTID}")) {
+            String msg = "You need to specify your Google ClientID " +
+                    "(in the GOOGLE_CLIENTID environment variable or in the \"google.clientid\" property of conf/application.conf file) " +
+                    "or you wont be able to log in!";
+            throw new ConfigurationException(msg);
+        }
+    }
+}

app/com/openseedbox/jobs/BootstrapNotGeneratedApplicationConf.java
@@ -1,0 +1,63 @@
+package com.openseedbox.jobs;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+import play.Logger;
+import play.Play;
+import play.exceptions.ConfigurationException;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.vfs.VirtualFile;
+
+import java.util.Properties;
+
+@OnApplicationStart
+public class BootstrapNotGeneratedApplicationConf extends Job {
+
+    private static final String NOT_GENERATED = "NOT_GENERATED";
+    private static final String APPLICATION_SECRET = "application.secret";
+    private static final String SESSION_COOKIE_NAME = "application.session.cookie";
+    private static final String INCLUDE_RUNTIME_CONF = "@include.runtime";
+
+    @Override
+    public void doJob() throws Exception {
+        Properties modifiedProps = new Properties();
+
+        // application.secret=NOT_GENERATED
+        if (Play.configuration.getProperty(APPLICATION_SECRET, "").equals(NOT_GENERATED)) {
+            Logger.info("Generating the secret key...");
+            modifiedProps.setProperty(APPLICATION_SECRET, RandomStringUtils.random(64, true, true));
+        }
+
+        // application.session.cookie=NOT_GENERATED
+        if (Play.configuration.getProperty(SESSION_COOKIE_NAME, "").equals(NOT_GENERATED)) {
+            Logger.info("Play session cookie name has not been generated; generating ... ");
+            String cookieName = ("PLAY_" + Play.configuration.getProperty("application.name","") + "_" +
+                    RandomStringUtils.random(16, true, false)).toUpperCase();
+            modifiedProps.setProperty(SESSION_COOKIE_NAME, cookieName);
+            Logger.info("Session cookie's name is now: %s", cookieName);
+        }
+
+        if (modifiedProps.size() > 0) {
+            String fileName = Play.configuration.getProperty("@include.runtime" , "");
+            if (fileName.isEmpty()) {
+                throw new ConfigurationException(String.format("NOT_GENERATED can only be used only with included configurations (see \"%s\" property)", INCLUDE_RUNTIME_CONF));
+            }
+            VirtualFile runtimeFile = VirtualFile.open(Play.applicationPath).child("conf/" + fileName);
+            if (runtimeFile.isDirectory()) {
+                throw new ConfigurationException(String.format("Included conf file %s is a directory (check \"%s\" property)", fileName, INCLUDE_RUNTIME_CONF));
+            }
+
+            Properties runtimeProps = new Properties();
+            if (runtimeFile.exists()) {
+                Logger.trace("Loading runtime configuration from existing %s", runtimeFile.getName());
+                runtimeProps.load(runtimeFile.inputstream());
+            }
+            runtimeProps.putAll(modifiedProps);
+            runtimeProps.store(runtimeFile.outputstream(), "");
+            Logger.debug("Saved generated properties to %s", runtimeFile.getName());
+
+            Play.configuration.putAll(modifiedProps);
+        }
+    }
+}

docker/Dockerfile
@@ -9,6 +9,7 @@ RUN bash -c "for repo in openseedbox{-common,}; do echo updating git for \$repo;
 RUN bash -c "for repo in openseedbox{-common,}; do echo updating play deps for \$repo; cd /src/\$repo; /play/play deps --sync --forProd; done"
 
 COPY application.conf /src/openseedbox/conf/application.conf
+RUN touch /src/openseedbox/conf/application.runtime.conf
 
 WORKDIR /src
 RUN /play/play war openseedbox -o war
@@ -84,6 +85,9 @@ ENV OPENSEEDBOX_JDBC_DRIVER=org.postgresql.Driver
 ENV OPENSEEDBOX_JDBC_USER=openseedbox
 ENV OPENSEEDBOX_JDBC_PASS=openseedbox
 ENV OPENSEEDBOX_CONTEXT_PATH=
+ENV OPENSEEDBOX_SESSION_COOKIE_NAME=NOT_GENERATED
+ENV APPLICATION_SECRET=NOT_GENERATED
+ENV GOOGLE_CLIENTID=
 
 # Copy WAR file from builder container to the final image
 COPY --from=builder /src/war $CATALINA_HOME/webapps/ROOT

docker/application.conf
@@ -36,6 +36,7 @@ openseedbox.zip.manifestonly=false
 openseedbox.context.path=${OPENSEEDBOX_CONTEXT_PATH}
 openseedbox.assets.prefix=${OPENSEEDBOX_CONTEXT_PATH}/public
 
+@include.runtime=application.runtime.conf
 
 # Test overrides
 %test.db=mem

docker/run.sh
@@ -1,45 +1,0 @@
-#!/bin/bash
-
-APPLICATION_SECRET=`grep "application.secret=NOT_GENERATED" conf/application.conf`
-GOOGLE_CLIENTID_NEEDED=`grep "google.clientid=" conf/application.conf`
-SESSION_COOKIE_NAME=`grep "application.session.cookie=NOT_GENERATED" conf/application.conf`
-
-if [ "$APPLICATION_SECRET"  != "" ]; then
-	echo "Play secret has not been generated; generating"
-	/play/play secret
-fi
-
-if [ "$GOOGLE_CLIENTID" == "" -a -n "$GOOGLE_CLIENTID_NEEDED" ]; then
-	echo "You need to specify your Google ClientID in the GOOGLE_CLIENTID environment variable or you wont be able to log in"
-	exit 1
-fi
-
-if [ "$SESSION_COOKIE_NAME"  != "" ]; then
-	echo -n "Play session cookie name has not been generated; generating ... "
-	application_name=`grep "application.name=" conf/application.conf | cut -d= -f2-`
-	new_cookie_name=`basename \`mktemp -u PLAY_${application_name}_XXXXXXXXXXXXXXXX\` | tr [a-z] [A-Z]`
-	sed -i -e s~$SESSION_COOKIE_NAME~application.session.cookie=$new_cookie_name~g conf/application.conf
-	echo "$new_cookie_name"
-fi
-
-echo "Starting play in $PWD"
-
-DOCKER_CGROUP=`basename $(head -n1 /proc/self/cgroup)`
-VOLUME_CGROUP_FILES=`ls /cgroup 2>/dev/null`
-SYSFS_CGROUP_FILES=`ls /sys/fs/cgroup 2>/dev/null`
-CGROUP_MOUNT="'-v /sys/fs/cgroup/:/cgroup:ro'"
-if [ -z "$SYSFS_CGROUP_FILES" ]; then
-	if [ -n "$VOLUME_CGROUP_FILES" ]; then
-		for cgroup_line in `find /cgroup -wholename *$DOCKER_CGROUP -type d` ;
-		do
-			cgroup_type=`echo $cgroup_line|cut -d/ -f3`
-			echo Setting cgroup limit: $cgroup_type
-			ln -s $cgroup_line /sys/fs/cgroup/$cgroup_type
-		done;
-	else
-		echo "WARNING: Could not enforce automatically cgroup memory limit to container"
-		echo "WARNING: Try running the container with $CGROUP_MOUNT option"
-	fi;
-fi;
-
-$JAVA_HOME/bin/java -XshowSettings -cp $(/play/play cp --silent|grep '^\[.*\]$'|sed -e "s/', '/:/g;s/\['//;s/'\]//") -Dapplication.path=. play.server.Server