c58a2b43409ab1ee50f50846b976ab81d4f94842
Szabolcs Hubai <szab.hu@gmail.com>
2023-11-19 15:03:19 +0100

    models/Torrent: add failing test for long zip download links (#117)

--- test/com/openseedbox/models/TestTorrent.java
+++ test/com/openseedbox/models/TestTorrent.java
@@ -10,10 +10,19 @@ public class TestTorrent extends UnitTest {
 	private String chartToAppend = "a";
 
 	String longName(int length) {
-		StringBuilder sb = new StringBuilder("This name is a little longer than ");
+		return longString("This name is a little longer than ", length);
+	}
+
+	String longLink(int length) {
+		return longString("my-awesome-openseedbox-backend-app-127-0-0-1.nip.io:9001/download/zip/", length);
+	}
+
+	String longString(String start, int length) {
+		StringBuilder sb = new StringBuilder(start);
 		sb.append(length).append(StringUtils.repeat(chartToAppend, length));
 		return sb.toString();
 	}
+
 
 	@Test
 	public void testTorrentNameShort() {
@@ -44,6 +53,42 @@ public class TestTorrent extends UnitTest {
 		Torrent t = new Torrent();
 		t.setName(longName(10000));
 		t.setHashString(DigestUtils.sha1Hex(t.getName()));
+		t.insertOrUpdate();
+	}
+
+	@Test
+	public void testTorrentZipDownloadLink100() {
+		Torrent t = new Torrent();
+		t.setName("Longer");
+		t.setHashString(DigestUtils.sha1Hex(t.getName()));
+		t.setZipDownloadLink(longLink(100));
+		t.insertOrUpdate();
+	}
+
+	@Test
+	public void testTorrentZipDownloadLink200() {
+		Torrent t = new Torrent();
+		t.setName("Even longer");
+		t.setHashString(DigestUtils.sha1Hex(t.getName()));
+		t.setZipDownloadLink(longLink(200));
+		t.insertOrUpdate();
+	}
+
+	@Test
+	public void testTorrentZipDownloadLink5000() {
+		Torrent t = new Torrent();
+		t.setName("The longest");
+		t.setHashString(DigestUtils.sha1Hex(t.getName()));
+		t.setZipDownloadLink(longLink(5000));
+		t.insertOrUpdate();
+	}
+
+	@Test
+	public void testTorrentZipDownloadLink10000() {
+		Torrent t = new Torrent();
+		t.setName("It's over 9000!");
+		t.setHashString(DigestUtils.sha1Hex(t.getName()));
+		t.setZipDownloadLink(longLink(10000));
 		t.insertOrUpdate();
 	}
 }
